fecf3f943105825fd3d7177765abb1555f85c682
marinalpo <malonsopoal@gmail.com>
2020-01-28 15:42:48 -0500

    Added Daux

---
+++ networks/deeplabv3_finetune.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+# Parameters
+num_class = 2  # FG vs BG
+batch_size = 8
+num_epochs = 2
+feature_extract = True  # True: only update reshaped layer params, False: finetune whole model
+input_size = 224
+
+
+# DataLoader
+data_dir = '/data/DAVIS/JPEGImages/480p/'
+# Data augmentation and normalization for training
+data_transforms = {
+    'train': transforms.Compose([
+        # transforms.RandomResizedCrop(input_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        # transforms.Resize(input_size),
+        transforms.CenterCrop(input_size),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+# Create training and validation datasets
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
+# Create training and validation dataloaders
+dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['train', 'val']}
+print(image_datasets)
+
+# Detect if we have a GPU available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+
+# def train_model(model, dataloaders, criterion, optimizer, num_epochs):
+#     val_acc_history = []
+#
+#     return model
+#
+#
+# def initialize_model(num_classes, feature_extract):
+#     model = torch.hub.load('pytorch/vision:v0.5.0', 'deeplabv3_resnet101', pretrained=True)
+#     # we only want to compute gradients for the newly initialized layer, not the others
+#     if feature_extract:
+#         for param in model.parameters():
+#             param.requires_grad = False
+#
+#     num_ftrs = model.fc.in_features
+#     model.fc = nn.Linear(num_ftrs, num_classes)
+#     return model
+#
+#
+# model = initialize_model(2, feature_extract)
+
--- networks/discriminator_aux.py
+++ networks/discriminator_aux.py
@@ -1,5 +1,66 @@
 import torch
-import torchvision
+from torchvision import models
+from PIL import Image
+from torchvision import transforms
+from torchvision.utils import save_image
+import matplotlib.pyplot as plt
 import numpy as np
+import requests
+from io import BytesIO
 
-model = torch.hub.load('pytorch/vision:v0.5.0', 'deeplabv3_resnet101', pretrained=True)
+net = torch.hub.load('pytorch/vision:v0.5.0', 'deeplabv3_resnet101', pretrained=True)
+# model2 = models.segmentation.deeplabv3_resnet101(pretrained=True)
+# model1.eval()
+
+def load_img_from_url(url):
+  response = requests.get(url)
+  img = Image.open(BytesIO(response.content))
+  return img
+
+
+def preprocess_img(img, device):
+  # img H x W x 3
+  transform = transforms.Compose([
+    transforms.Resize(256),  #  H' x W' x 3
+    transforms.ToTensor(),   #  3 x H' x W'
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+  ])
+
+  input_batch = transform(img)  # 1 x 3 x H' x W'
+  input_batch = input_batch.unsqueeze(0)
+  input_batch.to(device)
+  return input_batch
+
+
+def create_labels_palette(num_classes=21):
+  r = np.linspace(0, num_classes-1, num_classes)
+  g = np.linspace(0, 255, num_classes)
+  b = np.linspace(0, 255, num_classes//3).repeat(3)[:num_classes]
+  colors = np.stack([r,g,b],1).astype("uint8")
+  return colors
+
+
+def plot_labels(img_labels, palette):
+  img = Image.fromarray(img_labels.byte().cpu().numpy()).resize(input_image.size)
+  img.putpalette(palette)
+  plt.imshow(img)
+
+
+input_image = load_img_from_url('https://raw.githubusercontent.com/pytorch/hub/master/dog.jpg')
+filename = 'dog.jpg'
+filename = Image.open(filename)
+
+# preprocess img
+device = "cuda" if torch.cuda.is_available() else "cpu"
+input_batch = preprocess_img(filename, device)
+
+# estimate
+net.eval()
+estim = net(input_batch)['out'][0]  # num_classes x H x W
+estim_class = estim.argmax(0)       # H x W
+
+palette = create_labels_palette()
+img = Image.fromarray(estim_class.byte().cpu().numpy()).resize(input_image.size)
+img.putpalette(palette)
+
+img.save('ima5.png')
