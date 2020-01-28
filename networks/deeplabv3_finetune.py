443aaf7293ad56155bdb6fd31370ddd607e7eeac
marinalpo <malonsopoal@gmail.com>
2020-01-28 18:59:52 -0500

    Added deeplabv3

--- networks/deeplabv3_finetune.py
+++ networks/deeplabv3_finetune.py
@@ -17,32 +17,32 @@ feature_extract = True  # True: only update reshaped layer params, False: finetu
 input_size = 224
 
 
-# DataLoader
-data_dir = '/data/DAVIS/JPEGImages/480p/'
-# Data augmentation and normalization for training
-data_transforms = {
-    'train': transforms.Compose([
-        # transforms.RandomResizedCrop(input_size),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        # transforms.Resize(input_size),
-        transforms.CenterCrop(input_size),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
-
-# Create training and validation datasets
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
-# Create training and validation dataloaders
-dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['train', 'val']}
-print(image_datasets)
-
-# Detect if we have a GPU available
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# # DataLoader
+# data_dir = '/data/DAVIS/JPEGImages/480p/'
+# # Data augmentation and normalization for training
+# data_transforms = {
+#     'train': transforms.Compose([
+#         # transforms.RandomResizedCrop(input_size),
+#         transforms.RandomHorizontalFlip(),
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#     ]),
+#     'val': transforms.Compose([
+#         # transforms.Resize(input_size),
+#         transforms.CenterCrop(input_size),
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#     ]),
+# }
+#
+# # Create training and validation datasets
+# image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
+# # Create training and validation dataloaders
+# dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['train', 'val']}
+# print(image_datasets)
+#
+# # Detect if we have a GPU available
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 
---
+++ networks/discriminator.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+import numpy as np
+
+
+class Discriminator():  # Discriminator Network - PatchGAN
+    # Interessant: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/issues/39
+    def __init__(self, ):
+        super(Discriminator, self).__init__()
+
+
+    def forward(self, x):
+        return x
