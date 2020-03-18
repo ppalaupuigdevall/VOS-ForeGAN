88ec164e7b91959815dd90615d5065882858da5d
ppalaupuigdevall <ppalaupuigdevall@gmail.com>
2020-03-18 18:44:34 -0400

    Added remap for flows

--- scripts/warp_images.py
+++ scripts/warp_images.py
@@ -16,12 +16,24 @@ def resize_img(img, size):
     img_cv2_resized = cv2.cvtColor(np.array(img_pil_resized), cv2.COLOR_RGB2BGR)
     return img_cv2_resized
 
-def resize_flow(flow, size):
-    
-    return ''
+def resize_gray_img(img, size):
+    img_pil = Image.fromarray(img)
+    resi = transforms.Compose([transforms.Resize(size)])
+    img_pil_resized = resi(img_pil)
+    img_cv2_resized = np.array(img_pil_resized)
+    return img_cv2_resized
 
+def remap_values(values, xmin, xmax, ymin, ymax):
+    """
+    Remaps values with a positive straight line (es podria posar com a parametre si fos ppendent >0 o <0)
+    """
+    values = np.clip(values, xmin, xmax)
+    m = (ymax - ymin)/(xmax - xmin)
+    n = m * xmin
+    values = np.uint8(m * values + n)
+    return values
 
-#https:///GANimation/issues/22
+#https:///GANimation/issues/22 (utilitzen 128 x 128)
 
 resolutions = {0:(140,260), 1:(175,325), 2:(224,416), 3:(280, 520)}
 
@@ -32,39 +44,30 @@ img_name = '00000.jpg'
 flo_name = '000000.flo'
 
 flow = readFlow(os.path.join(flo_dir, flo_name))
-# Jo crec que el millor que es pot fer es en comptes de normalitzar el flow entre -1 i 1 (dividint entre 448,832), 
-# dividir entre 255 per deixar-ho mes o menys al nivel de la imatge (com fan al lucid)
-u = flow[:,:,0]/255.0
-v = flow[:,:,1]/255.0
-u_max = u/np.max(np.abs(u)) 
-v_max = v/np.max(np.abs(v))
-
-
-
-print(np.mean(u/448))
-print(np.mean(v/832))
-# u_128 = np.uint8((u + 1.0)*255)
-# cv2.imshow('u',u_128)
-# cv2.waitKey(0)
-fig, ax = plt.subplots(nrows=1, ncols=2)
-ax[0].imshow(u_max)
-ax[1].imshow(v_max)
-plt.show()
-
 img = cv2.imread(os.path.join(img_dir, img_name))
 img_warped = warp_flow(img, flow)
 
-print(img.shape)
-print(img_warped.shape)
-print(flow.shape)
+u = flow[:,:,0]
+v = flow[:,:,1]
+flow_u_remaped = remap_values(u, -20, 20, 0, 255)
+flow_v_remaped = remap_values(v, -20, 20, 0, 255)
 
 # Resize ori to warped's shape
 desired_shape = img_warped.shape[:2]
-desired_shape = resolutions[2]
-
+desired_shape = resolutions[1]
 img_ori_resized = resize_img(img, desired_shape)
 img_warped_resized = resize_img(img_warped,desired_shape)
+flow_u_remaped_resized = resize_gray_img(flow_u_remaped, desired_shape)
 
-cv2.imwrite('./warp1.jpg', img_warped)
-cv2.imwrite('./img_warped_resized.jpg', img_warped_resized)
-cv2.imwrite('./img_ori_resized.jpg',img_ori_resized)
+display, save = False, False
+
+if(display):
+    cv2.imshow('v', flow_v_remaped)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if(save):
+    cv2.imwrite('./warp1.jpg', img_warped)
+    cv2.imwrite('./img_warped_resized.jpg', img_warped_resized)
+    cv2.imwrite('./img_ori_resized.jpg',img_ori_resized)
+    cv2.imwrite('./flow_u_resized.jpg', flow_u_remaped_resized)
